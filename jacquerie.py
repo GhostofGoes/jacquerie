7969a96e65e9ebaaf701afcae58377f820f59b9f
Christopher Goes <ghostofgoes@gmail.com>
2017-02-04 22:48:58 -0800

    build_message function

--- jacquerie.py
+++ jacquerie.py
@@ -16,6 +16,35 @@ def deserialize(message):
     return loads(message)
 
 
+def build_message(message, crypt):
+    """
+    Builds a message to send to a group
+    :param message: Plaintext of message being sent
+    :param crypt: Crypto class object
+    :return: Serialized packet containing: encrypted payload, nonce, signature
+    """
+    # Ultimately, a message is composed of thus:
+    #   Plaintext message
+    #   Timestamp in Unix Epoch time
+    #   Signature (timestamp encrypted with private key)
+    #   Nonce
+
+    plaintext = message
+    timestamp = str(time())  # Unix Epoch time
+    packed_message = serialize({"message": plaintext, "timestamp": timestamp})
+
+    # As sent across wire:
+    #   Encrypted payload
+    #   Nonce
+    #   Signature
+    enc = crypt.encrypt(packed_message)
+    package = {}
+    package["payload"] = enc[0]
+    package["nonce"] = enc[1]
+    package["signature"] = crypt.gen_signature(timestamp)
+    return serialize(package)
+
+
 def main():
     crypt = Crypto(seed=get_password("Enter your private key: "),
                    group_key=get_password("Enter the group key of length {}: ".format(lol.SecretBox.KEY_SIZE)))
@@ -23,29 +52,8 @@ def main():
     blacklist = {}  # List of signatures that we drop message from
 
     while chatting:
-        # Ultimately, a message is composed of thus:
-        #   Plaintext message
-        #   Timestamp in Unix Epoch time
-        #   Signature (timestamp encrypted with private key)
-        #   Nonce
-
         plaintext = str(get_chat_message(private=False))  # Unicode String
-        timestamp = str(time())  # Unix Epoch time
-        packed_message = serialize({"message": plaintext, "timestamp": timestamp})
-
-        # As sent across wire:
-        #   Encrypted payload
-        #   Nonce
-        #   Signature
-        enc = crypt.encrypt(packed_message)
-        package = {}
-        package["payload"] = enc[0]
-        package["nonce"] = enc[1]
-        package["signature"] = crypt.gen_signature(timestamp)
-
-        packet = serialize(package)
-
-
+        packet = build_message(message=plaintext, crypt=crypt)
 
 
 if __name__ == '__main__':
