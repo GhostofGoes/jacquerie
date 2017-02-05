211cd5c53f89e9afecebbf7cc39e9eaa928e9afa
Christopher Goes <ghostofgoes@gmail.com>
2017-02-04 22:56:31 -0800

    Message blacklists

--- jacquerie.py
+++ jacquerie.py
@@ -6,6 +6,7 @@ import nacl.secret as lol
 from input import get_chat_message, get_password
 from json import loads, dumps
 from time import time
+from com import send_message, get_message
 
 
 def serialize(message):
@@ -45,6 +46,21 @@ def build_message(message, crypt):
     return serialize(package)
 
 
+def unpack_message(message, crypt, blacklist):
+    """
+    Unpacks a received message
+    :param message: Serialized message
+    :param crypt: Crypto class object
+    :param blacklist: Dictionary of signatures to ignore
+    :return: Deserialized message as dict containing: message, timestamp, signature, (HANDLE??), (LETTER NAME??)
+    """
+    package = deserialize(message)
+    if package["signature"] in blacklist:
+        print("(DEBUG) Invalid message received") # TODO: debugging
+        return {}  # EMPTY DICT FOR INVALID MESSAGES
+
+
+
 def main():
     crypt = Crypto(seed=get_password("Enter your private key: "),
                    group_key=get_password("Enter the group key of length {}: ".format(lol.SecretBox.KEY_SIZE)))
@@ -52,8 +68,11 @@ def main():
     blacklist = {}  # List of signatures that we drop message from
 
     while chatting:
+        # TODO: ability for user to set handle
+        # TODO: chat output format
         plaintext = str(get_chat_message(private=False))  # Unicode String
         packet = build_message(message=plaintext, crypt=crypt)
+        send_message(packet)
 
 
 if __name__ == '__main__':
