61ba2d1a548d9dbe233acdbf2514be8a71bf56f2
Christopher Goes <ghostofgoes@gmail.com>
2017-02-04 22:31:25 -0800

    Added a COM file

---
+++ com.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# TODO: use asyncio?
+def send_message(message):
+    pass
+
+
+# TODO: use asyncio?
+def get_message():
+    message = {}
+    return message
